858e97b0a8ab891f690a11a7b36572063affdb8a
peach <amoralyse21@gmail.com>
2023-07-03 00:57:14 +0800

    gamay update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BannerImage from "../assets/pizza.jpeg";
+import Wallpaper from "../assets/home.png";
 import "../styles/Home.css";
 
 function Home() {
   return (
-    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+    <div className="home" style={{ backgroundImage: `url(${Wallpaper})` }}>
       <div className="headerContainer">
-        <h1> Pedro's Pizzeria </h1>
-        <p> PIZZA TO FIT ANY TASTE</p>
+        <h1> Kathy's Cake Shop </h1>
         <Link to="/menu">
           <button> ORDER NOW </button>
         </Link>
